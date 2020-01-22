ea0beeeedbd9eaaa051f45a6cd6c87152d32d234
HaeJun Seo <kan02134@gmail.com>
2020-01-22 14:17:37 +0900

    Feat: Add mock-data plugin

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
+import yamlData from './plugins/yaml-data';
 import '@babel/polyfill';
 
 Vue.config.productionTip = false;
+Vue.use(yamlData);
 
 new Vue({
   router,
---
+++ src/plugins/yaml-data.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import yaml from 'yaml';
+
+export default {
+  async install(Vue) {
+    // eslint-disable-next-line no-param-reassign
+    Vue.prototype.$yaml = new Promise(async (resolve) => {
+      const { data } = await axios.get('/mock-data.yml');
+      resolve(yaml.parse(data));
+    });
+  },
+};
