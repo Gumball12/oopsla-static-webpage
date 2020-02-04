2812b151051b5dd3a3752abb8dbf405a89f8bb07
HaeJun Seo <kan02134@gmail.com>
2020-02-05 02:44:12 +0900

    Feat: Create PNF page

---
+++ src/views/PNF.vue
@@ -0,0 +1,6 @@
+<template>
+  <v-container class="d-flex fill-height flex-column justify-center">
+    <p class="display-1 font-weight-bold">Page Not Found</p>
+    <p class="display-1 grey--text">OPPS!..A</p>
+  </v-container>
+</template>
