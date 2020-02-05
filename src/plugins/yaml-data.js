c6768d0d84631d267eeda3f7a7d34f2f3dc233ad
Gumball12 <kan02134@gmail.com>
2020-02-05 16:58:52 +0900

    Docs: Change yaml file name

--- src/plugins/yaml-data.js
+++ src/plugins/yaml-data.js
@@ -5,7 +5,7 @@ export default {
   async install(Vue) {
     // eslint-disable-next-line no-param-reassign
     Vue.prototype.$yaml = new Promise(async (resolve) => {
-      const { data } = await axios.get('/mock-data.yml');
+      const { data } = await axios.get('/data.yml');
       resolve(yaml.parse(data));
     });
   },
