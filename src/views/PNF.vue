030003c6e43e133260f3ac03fcaf0c002c2c2110
Gumball12 <kan02134@gmail.com>
2020-02-05 16:52:28 +0900

    Feat: Update style

--- src/views/PNF.vue
+++ src/views/PNF.vue
@@ -1,6 +1,12 @@
 <template>
   <v-container class="d-flex fill-height flex-column justify-center">
     <p class="display-1 font-weight-bold">Page Not Found</p>
-    <p class="display-1 grey--text">OPPS!..A</p>
+    <p class="display-1 grey--text">OOPS<span class="letter-l">!.</span>.A</p>
   </v-container>
 </template>
+
+<style lang="scss" scoped>
+span.letter-l {
+  letter-spacing: -3px;
+}
+</style>
